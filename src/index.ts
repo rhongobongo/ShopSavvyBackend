c206c671e4716b07921a7957ed964c61de96dde9
rhongobongo <epicjeff03@gmail.com>
2024-06-26 10:44:55 +0800

    -Added cors

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,25 @@
 import express from 'express';
 import pkg from 'body-parser';
 const { json } = pkg;
+import cors from 'cors';
 import { userRoutes } from './routes/UserRoutes.js';
 import { authRoutes } from './routes/AuthRoutes.js';
 import swaggerJSDoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
 import swaggerConfig from './swaggerConfig.js';
 
+
+
 const app = express();
 const port = 3001;
 
 // Middleware
 app.use(json());
+app.use(cors({
+  origin: 'http://localhost:3000', // Allow requests from frontend React app
+  credentials: true, // Enable CORS credentials (cookies, authorization headers)
+}));
+
 
 // Routes
 app.use('/api/users', userRoutes); // Changed to `/api/users` for clarity
